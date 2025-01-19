a42e2c3af384971106b99553a8b2595aca4030e6
wonza-hub <wonza8585@gmail.com>
2025-01-19 22:24:48 +0900

    test: 사진 게시판 목록 조회 목업 추가

---
+++ mock/api/photoAlbum.ts
@@ -0,0 +1,28 @@
+import MockAdapter from "axios-mock-adapter";
+import photoAlbumData from "../json/photoAlbum.json";
+
+// MOCKUP: 사진게시판
+const mockPhotoAlbum = (mock: MockAdapter) => {
+  // 전체 목록 조회
+  mock.onGet("/api/photo-post").reply((config) => {
+    const { page } = config.params;
+    const PAGE_SIZE = 10;
+
+    const startIdx = (page - 1) * PAGE_SIZE;
+    const endIdx = page * PAGE_SIZE;
+    const paginatedItems = photoAlbumData["photo-album"].slice(
+      startIdx,
+      endIdx
+    );
+
+    return [
+      200,
+      {
+        response: {
+          dtoList: paginatedItems,
+        },
+      },
+    ];
+  });
+};
+export default mockPhotoAlbum;
