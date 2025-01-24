774b77645c3f6e82189ba3e03d75d63a17fa9532
wonza-hub <wonza8585@gmail.com>
2025-01-24 19:33:46 +0900

    chore: locatorjs 확장 프로그램 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import setupLocatorUI from '@locator/runtime';
+
+if (import.meta.env.DEV) {
+    setupLocatorUI();
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
